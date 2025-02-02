1b608f089aee8ba6bc1712a45986c24290a4376b
chaoji <jifc17@outlook.com>
2025-02-02 21:06:38 +0800

    change ytb audio extractor to yt_dlp

--- bot/plugins/youtube_audio_extractor.py
+++ bot/plugins/youtube_audio_extractor.py
@@ -2,7 +2,7 @@ import logging
 import re
 from typing import Dict
 
-from pytube import YouTube
+import yt_dlp
 
 from .plugin import Plugin
 
@@ -18,11 +18,11 @@ class YouTubeAudioExtractorPlugin(Plugin):
     def get_spec(self) -> [Dict]:
         return [{
             "name": "extract_youtube_audio",
-            "description": "Extract audio from a YouTube video",
+            "description": "Extract audio from a video from websites like YouTube, bilibili and others",
             "parameters": {
                 "type": "object",
                 "properties": {
-                    "youtube_link": {"type": "string", "description": "YouTube video link to extract audio from"}
+                    "youtube_link": {"type": "string", "description": "video link to extract audio from"}
                 },
                 "required": ["youtube_link"],
             },
@@ -31,10 +31,23 @@ class YouTubeAudioExtractorPlugin(Plugin):
     async def execute(self, function_name, helper, **kwargs) -> Dict:
         link = kwargs['youtube_link']
         try:
-            video = YouTube(link)
-            audio = video.streams.filter(only_audio=True, file_extension='mp4').first()
-            output = re.sub(r'[^\w\-_\. ]', '_', video.title) + '.mp3'
-            audio.download(filename=output)
+            ydl_opts = {
+                'outtmpl': '.cache/%(title)s.%(ext)s',  # 设置输出文件名格式
+                'format': 'bestaudio/best',  # 下载最佳质量的视频
+                'quiet': True,  # 不显示下载进度
+                'embedthumbnail': True,  # 将缩略图嵌入到音频文件中
+                'addmetadata': True,  # 将元数据添加到音频文件中
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                # 提取视频信息
+                info_dict = ydl.extract_info(link, download=False)
+
+                # 获取视频的标题和文件名
+                title = info_dict.get('title', 'Unknown Title')
+                output = ydl.prepare_filename(info_dict)
+                ydl.download(link)
+
+            # audio.download(filename=output)
             return {
                 'direct_result': {
                     'kind': 'file',
